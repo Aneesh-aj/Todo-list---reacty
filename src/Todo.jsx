841d9f436a7eac50b422d623057c1657c8d5bd80
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2023-12-02 21:30:02 +0530

    new commit

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -10,21 +10,21 @@ function Todo() {
 
   const [Todo, setTodo] = useState('')
   const [Todos, setTodos] = useState([])
-  const [EditId,setEditId] = useState(0)
+  const [EditId, setEditId] = useState(0)
   console.log('todo', Todo)
 
   const addTodo = () => {
-     if(Todo !==''){
-      setTodos([...Todos, { list:Todo, id: Date.now(), status : false}])
+    if (Todo !== '') {
+      setTodos([...Todos, { list: Todo, id: Date.now(), status: false }])
       setTodo('')
-     }
-     if(EditId){
-        const edittodo = Todos.find((todo)=>todo.id === EditId)
-        const updatetodo = Todos.map((todo)=>todo.id === edittodo.id ?(todo = {id:todo.id,list:Todo}) :(todo={id:todo.id ,list : todo.list}))
-        setTodos(updatetodo)
-        setEditId(0)
-        setTodo('')
-     }
+    }
+    if (EditId) {
+      const edittodo = Todos.find((todo) => todo.id === EditId)
+      const updatetodo = Todos.map((todo) => todo.id === edittodo.id ? (todo = { id: todo.id, list: Todo }) : (todo = { id: todo.id, list: todo.list }))
+      setTodos(updatetodo)
+      setEditId(0)
+      setTodo('')
+    }
   }
 
   const submitng = (event) => {
@@ -35,29 +35,29 @@ function Todo() {
 
   useEffect(() => {
     inputRef.current.focus
-  }) 
+  })
 
   const onDelete = (id) => {
-    setTodos( Todos.filter((todo) => todo.id !== id))
+    setTodos(Todos.filter((todo) => todo.id !== id))
   }
 
-  const onCompelete=(id)=>{
-      let complete = Todos.map((todo)=>{
-         if(todo.id === id){
-            return ({...todo,status:!todo.status})
-         }
-         return todo
-      })
+  const onCompelete = (id) => {
+    let complete = Todos.map((todo) => {
+      if (todo.id === id) {
+        return ({ ...todo, status: !todo.status })
+      }
+      return todo
+    })
 
-      setTodos(complete)
+    setTodos(complete)
   }
 
-  const onEdit=(id)=>{
-      
-   const edittodo = Todos.find((todo)=>todo.id === id)
-   setTodo(edittodo.list)
-   setEditId(edittodo.id)
-     
+  const onEdit = (id) => {
+
+    const edittodo = Todos.find((todo) => todo.id === id)
+    setTodo(edittodo.list)
+    setEditId(edittodo.id)
+
   }
 
 
@@ -66,24 +66,24 @@ function Todo() {
       <h2>TODO APP</h2>
       <form action="" className="form-group" onSubmit={submitng}>
         <input type="text" ref={inputRef} value={Todo} onChange={(event) => setTodo(event.target.value)} placeholder='Enter you todo list' />
-        <button onClick={addTodo}>{EditId ? 'Edit':'Add'}</button>
+        <button onClick={addTodo}>{EditId ? 'Edit' : 'Add'}</button>
       </form>
       <div className='list'>
         <ul>
           {Todos.map((todo) => (
             <li className='list-items' >
-              <div className='list-item-list' id={todo.status?'list-item':null}>{todo.list}</div>
+              <div className='list-item-list' id={todo.status ? 'list-item' : null}>{todo.list}</div>
               <span>
                 <IoMdDoneAll className='list-item-icons'
-                 id='complete'
-                 title='complete'
-                 onClick={()=> onCompelete(todo.id)}
-                  />
+                  id='complete'
+                  title='complete'
+                  onClick={() => onCompelete(todo.id)}
+                />
                 <FiEdit className='list-item-icons'
-                 id='edit' 
-                 title='edit'
-                 onClick={()=>onEdit(todo.id)}
-                 />
+                  id='edit'
+                  title='edit'
+                  onClick={() => onEdit(todo.id)}
+                />
                 <MdDelete className='list-item-icons'
                   id='delete'
                   title='delete'
@@ -94,7 +94,6 @@ function Todo() {
           ))}
         </ul>
       </div>
-
     </div>
   )
 }
